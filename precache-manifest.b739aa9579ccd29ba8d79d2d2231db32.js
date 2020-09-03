717d30a8637d1af0b7308c2533f0f3e6ecc98cae
Makwana Vishal <70061350+vishal5282@users.noreply.github.com>
2020-09-03 23:16:08 +0530

    Updates

---
+++ precache-manifest.b739aa9579ccd29ba8d79d2d2231db32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46fee786a83927a1933b2541a08e584",
+    "url": "/React_first/index.html"
+  },
+  {
+    "revision": "f47430a4591699dc8fbb",
+    "url": "/React_first/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6fc47fee98a17a551000",
+    "url": "/React_first/static/css/main.a5b313ab.chunk.css"
+  },
+  {
+    "revision": "f47430a4591699dc8fbb",
+    "url": "/React_first/static/js/2.22bcd927.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_first/static/js/2.22bcd927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc47fee98a17a551000",
+    "url": "/React_first/static/js/main.a78bddc4.chunk.js"
+  },
+  {
+    "revision": "5e6791e572e4dd94fbae",
+    "url": "/React_first/static/js/runtime-main.16e72685.js"
+  }
+]);
